4e45e75a5ef6f0238276406c1ef2597fed70142e
Jon Durbin <jond@greenviewdata.com>
2012-05-14 10:52:00 -0400

    Moved from defines to enums, updated some function declarations and other misc changes.

--- dre2.h
+++ dre2.h
@@ -5,33 +5,55 @@
 
 #define RANGE 256
 #define DRE2_INFINITE -1
-#define DRE2_LEFT 0
-#define DRE2_RIGHT 1
-#define DRE2_GROUP_OPEN -1
-#define DRE2_GROUP_CLOSE -2
+
+enum
+DRE2_DIRECTION
+{
+  DRE2_LEFT,
+  DRE2_RIGHT,
+};
 
 // Character classes and predifined classes.
-#define DRE2_CHAR_CLASS -3
-#define DRE2_EOF -4
-#define DRE2_ALPHA -5
-#define DRE2_BORDER -6
-#define DRE2_DIGIT -7
-#define DRE2_SIM_DIGIT -8
-#define DRE2_OBF_DIGIT -9
-#define DRE2_HEX -10
-#define DRE2_URL -11
-#define DRE2_DOMAIN -12
-#define DRE2_WORD -13
-#define DRE2_WHITE_SPACE -14
-#define DRE2_8BIT -15
+enum
+DRE2_NODE_TYPE
+{
+  DRE2_GROUP_OPEN = -1,
+  DRE2_GROUP_CLOSE = -2,
+  DRE2_CHAR_CLASS = -3,
+  DRE2_EOF = -4,
+  DRE2_ALPHA = -5,
+  DRE2_BORDER = -6,
+  DRE2_DIGIT = -7,
+  DRE2_SIM_DIGIT = -8,
+  DRE2_OBF_DIGIT = -9,
+  DRE2_HEX = -10,
+  DRE2_URL = -11,
+  DRE2_DOMAIN = -12,
+  DRE2_WORD = -13,
+  DRE2_WHITE_SPACE = -14,
+  DRE2_8BIT = -15,
+};
 
 // Matching methods.
-#define DRE2_SN_SC 0
-#define DRE2_SN_MC 1
-#define DRE2_SN_SC_H 2
-#define DRE2_SN_MC_H 3
-#define DRE2_MN 4
+enum
+DRE2_MATCH_METHOD
+{
+  DRE2_SN_SC,                    // Match using a single starting node, singe first possible character.
+  DRE2_SN_MC,                    // Match using a single starting node, multiple first possible character.
+  DRE2_SN_SC_H,                  // Match using a single starting node, single possible first character, using horspool-ish skip-table.
+  DRE2_SN_MC_H,                  // Match using a single starting node, multiple first possible characters, using horspool-ish skip-table.
+  DRE2_MN,                       // Match from multiple starting nodes.
+};
 
+// Matching/parsing options.
+enum
+DRE2_OPTION
+{
+  DRE2_GREEDY = 1,               // Greedy match, used when we want info about the string that matched the regex.
+  DRE2_EXACT = 1 << 1,           // Exact match, e.g. 'abc' matches 'abc' but not 'abcd'
+  DRE2_NO_CASE = 1 << 2,         // Ignore case, e.g. 'a' matches 'a' or 'A'.
+  DRE2_SUBMATCH = 1 << 3,        // Keep the original digraph so submatch extraction can take place.
+};
 
 #ifndef true
   #define true 1
@@ -94,9 +116,6 @@ dre2 {
   struct dre2_node *v;           // Vertices within the graph.
   int count;                     // Number of nodes in the graph.
   int skip_table[RANGE];         // Horspool style skip table.
-  int *state;                    // State table.
-  int *reachable;                // Un-lookup table version of the state table.
-  int *r_temp;                   // Temp reachable table.
   int min_length;                // Minimum string length that can possibly match this RE.
   int starting_point;            // Best starting point to reduce number of checks.
   int *starting_chars;           // Characters possible at our starting point.
@@ -105,17 +124,18 @@ dre2 {
   unsigned char c;               // Single char to search for if there's a single starting char.
   int *starting_points;          // If all else fails, match from multiple nodes using best node in each path.
   int starting_count;            // Number of nodes for above.
+  int options;                   // Options, e.g. greedy, ignore case, etc.
 };
 
 
 // Function definitions.
 int dre2_char_matches( struct dre2_node *node, unsigned char c );
-unsigned char * dre2_matcher( struct dre2 *graph, unsigned char *begin_ptr, unsigned char *input, int start, int direction, int length );
-int dre2_sn_sc_horspool( struct dre2 *graph, unsigned char *input, int length );
-int dre2_sn_sc( struct dre2 *graph, unsigned char *input, int length );
-int dre2_sn_mc_horspool( struct dre2 *graph, unsigned char *input, int length );
-int dre2_sn_mc( struct dre2 *graph, unsigned char *input, int length );
-int dre2_mn( struct dre2 *graph, unsigned char *input, int length );
+unsigned char * dre2_matcher( struct dre2 *graph, unsigned char *begin_ptr, unsigned char *input, int start, int direction, int length, int *r_temp, int *reachable );
+int dre2_sn_sc_horspool( struct dre2 *graph, unsigned char *input, int length, int *r_temp, int *reachable );
+int dre2_sn_sc( struct dre2 *graph, unsigned char *input, int length, int *r_temp, int *reachable );
+int dre2_sn_mc_horspool( struct dre2 *graph, unsigned char *input, int length, int *r_temp, int *reachable );
+int dre2_sn_mc( struct dre2 *graph, unsigned char *input, int length, int *r_temp, int *reachable );
+int dre2_mn( struct dre2 *graph, unsigned char *input, int length, int *r_temp, int *reachable );
 int dre2_match( struct dre2 *graph, unsigned char *input );
 unsigned char * dre2_escaped( unsigned char *re );
 int dre2_binsearch( int *values, int min, int max, int key );
@@ -143,7 +163,7 @@ int dre2_use_paths( struct dre2 *graph, int best, struct dre2_fl_cost *cost );
 int dre2_first_or_last( struct dre2 *graph, struct dre2_fl_cost *cost );
 void dre2_remove_minimal( int **minimal, int id );
 void dre2_add_minimal( int **minimal, int *node_count );
-void dre2_add_node( struct dre2_node **v, int *node_count, int c, int **minimal );
+void dre2_add_node( struct dre2_node **v, int *node_count, int c, int **minimal, int no_realloc );
 void dre2_add_neighbor( struct dre2_node **v, int origin, int dest );
 void dre2_add_parents( struct dre2 *graph );
 void dre2_skip_table( struct dre2 *graph );
@@ -151,6 +171,6 @@ void dre2_duplicate_group( struct dre2_node **v, int *node_count, int *last_node
 void dre2_duplicate_node( struct dre2_node **v, int *node_count, int last_node, int **minimal );
 void dre2_make_range( struct dre2_node **v, int *node_count, int *last_node, struct dre2_parse_return *res, int min, int max, int **minimal );
 struct dre2_parse_return dre2_parse_recursive( struct dre2_node **v, int *node_count, unsigned char *re, int length, int pos, int **minimal );
-struct dre2 dre2_parse( unsigned char *re );
+struct dre2 * dre2_parse( unsigned char *re, int options );
 void print_dre2( struct dre2 *graph );
 void print_reverse_dre2( struct dre2 *graph );
