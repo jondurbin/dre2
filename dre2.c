c1187f54f5e5c25b536642796b7f56a346849d71
Jon Durbin <jond@greenviewdata.com>
2012-05-14 10:51:23 -0400

    Updated sample program to use new syntax.

--- dre2.c
+++ dre2.c
@@ -3,31 +3,37 @@
 int
 main( int argc, char *argv[] )
 {
+  struct dre2 *re; // Pointer to regex digraph object.
+  char *buf;       // Buffer to hold input strings.
+
   if ( argc != 2 )
   {
     printf( "Usage: cat file.txt | %s <regex>\n", argv[0] );
     return 0;
   }
-  struct dre2 graph;
-  graph = dre2_parse( ( unsigned char * )argv[1] );
-  if ( graph.v == NULL )
+
+  // Parse the regex string into the dre2 object.
+  re = dre2_parse( ( unsigned char * )argv[1], 0 );
+
+  // Make sure parsing was successful.
+  if ( re == NULL )
   {
     printf( "Failed to parse!\n" );
     return 0;
   }
 
-  char *buf;
+  // Allocate some memory for input strings from stdin.
   buf = ( unsigned char * )calloc( 0x10000, 1 );
-  int count = 0;
+
+  // Check if input strings match the regex.
   while ( fgets( buf, 0x10000 - 1, stdin ) )
   {
-    if ( dre2_match( &graph, buf ) )
-    {
+    if ( dre2_match( re, buf ) )
       printf( "%s", buf );
-      count++;
-    }
   }
-  cleanup_dre2( &graph );
-  free( buf );
+
+  // Cleanup memory.
+  cleanup_dre2( re );
+  free( buf ); buf = NULL;
   return 0;
 }
