2610cfbd6d801668e0c175ab110c484b2512211c
Jon Durbin <jond@greenviewdata.com>
2012-05-17 11:28:10 -0400

    Removed some unnecessary variables from re2.cpp

--- re2.cpp
+++ re2.cpp
@@ -9,20 +9,17 @@ using namespace std;
 using namespace re2;
 
 int main(int argc, char *argv[]) {
+  char *buf;
   if (argc != 2) {
     printf("Usage: cat testfile | ./%s <regex to evaluate>\n", argv[0]);
     return 0;
   }
 
   RE2 search_re(argv[1]);
-  double res;
 
-  char *buf;
   buf = (char*)calloc(0x10000, 1);
-  int count = 0;
   while (fgets(buf, 0x10000 - 1, stdin)) {
     if (RE2::PartialMatch(buf, search_re)) {
-      count++;
       printf( "%s", buf );
     }
   }
