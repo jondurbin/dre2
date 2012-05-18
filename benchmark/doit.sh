a04671f8810f5a990084c707339e47b8f07b027f
Jon Durbin <jond@greenviewdata.com>
2012-05-18 17:05:28 -0400

    More regexes to test in benchmarks.

--- benchmark/doit.sh
+++ benchmark/doit.sh
@@ -3,7 +3,7 @@
 # Function which runs a regex against the test file and records count and time.
 run_regex() {
 	start_time=$(date +%s.%N)
-	count=`cat mtent12.txt | ./xees/$1 $2 | wc -l`
+	count=`cat mtent12.txt | "./xees/$1" "$2" | wc -l`
 	end_time=$(date +%s.%N)
 	time_diff=$(echo "$end_time - $start_time" | bc)
 	printf "%10s | %10s | %10s\n" $1 $count $time_diff
@@ -21,7 +21,7 @@ if [ ! -e "xees/dre2" ]; then
 fi
 
 # Iterate through regular expressions.
-for re in `cat regexes.txt`
+cat regexes.txt | while read re
 do
 	echo Regex: $re;
 	echo =================================================
@@ -31,7 +31,7 @@ do
 	# Iterate through various regex programs.
 	for xee in `ls xees`
 	do
-		run_regex $xee $re
+		run_regex "$xee" "$re"
 		echo -------------------------------------------------
 	done
 	echo
